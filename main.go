088e309161b52b337d27f3485cbb70c7bdd131ff
Vikas Poddar <poddarvikas685@gmail.com>
2024-12-15 13:42:47 +0530

    Add instruction how demo works & Rename packages

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import external_package "github.com/VikasPoddar/go-in-1hr/external-package"
+import "github.com/VikasPoddar/go-in-1hr/external_package"
 
 func main() {
+	// main package act a entry-point to execution
+	// every demo has its own package
+	// for example
+	// 1. testing_demo package: This package demonstrate how simple testing works in go
 	external_package.UUIDDemo()
 }
