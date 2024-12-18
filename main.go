76d72b997c343f19361e2510a0cec20006245dd1
Vikas Poddar <poddarvikas685@gmail.com>
2024-12-18 21:41:03 +0530

    add commit siging with ssh-key

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ func main() {
 	// every demo has its own package
 	// for example
 	// 1. testing_demo package: This package demonstrate how simple testing works in go
+	os_package.IoDemo()
 	os_package.OsDemo()
 }
 
