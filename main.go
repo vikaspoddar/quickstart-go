c91581ac2b27f45747f0a01dea0f6696fbde60d0
VikasPoddar <poddarvikas685@gmail.com>
2025-01-25 06:49:31 +0000

    docs: fix docs

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	// main package act entry-point to execution
+	// main package id entry-point to execution
 	// every demo has its own package
 	// for example
 	// 1. testing package: This package demonstrate how simple testing works in go
